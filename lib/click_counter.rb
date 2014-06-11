8ab3b8ce12452e535ea78ea892b4e3b86aa1b825
Daniel Irvine <dirvine@8thlight.com>
2014-06-11 17:43:00 +0100

    spec and impl for displaying a label of zero

--- lib/click_counter.rb
+++ lib/click_counter.rb
@@ -2,4 +2,10 @@ require 'qt'
 
 class ClickCounter < Qt::Widget
 
+  def initialize
+    super(nil)
+    layout = Qt::VBoxLayout.new
+    layout.addWidget(Qt::Label.new('0'))
+    setLayout(layout)
+  end
 end
--- spec/click_counter_spec.rb
+++ spec/click_counter_spec.rb
@@ -2,12 +2,28 @@ require 'spec_helper'
 require 'qt'
 require 'click_counter'
 
+RSpec::Matchers.define :have_label_with_text do |expected|
+  match do |widget|
+    widget.children.any? do |child|
+      child.kind_of?(Qt::Label) and child.text==expected
+    end
+  end
+end
+
 describe ClickCounter do
 
-  it 'is a window' do
+  let(:window) { ClickCounter.new }
+
+  before do
     app = Qt::Application.new(ARGV)
-    window = ClickCounter.new
+  end
+
+  it 'is a window' do
     expect(window).to be_kind_of(Qt::Widget)
     expect(window.parent).to be_nil
+  end
+  
+  it 'shows 0 before any clicks are made' do
+    expect(window).to have_label_with_text('0')
   end
 end
