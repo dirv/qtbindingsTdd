0ed049dcaca8d4ab177d221d3446c0b3b6060d98
Daniel Irvine <dirvine@8thlight.com>
2014-06-11 17:12:12 +0100

    make first spec pass

---
+++ lib/click_counter.rb
@@ -0,0 +1,5 @@
+require 'qt'
+
+class ClickCounter < Qt::Widget
+
+end
--- spec/click_counter_spec.rb
+++ spec/click_counter_spec.rb
@@ -1,7 +1,13 @@
+require 'spec_helper'
+require 'qt'
+require 'click_counter'
+
 describe ClickCounter do
 
-  it 'displays a window' do
+  it 'is a window' do
+    app = Qt::Application.new(ARGV)
     window = ClickCounter.new
-    expect(window).to be_kind_of(Qt::Window)
+    expect(window).to be_kind_of(Qt::Widget)
+    expect(window.parent).to be_nil
   end
 end
